ca8fe709fadc6b55941f5504905339145c80eead
Anuj Gupta <149232244+anujgupta845@users.noreply.github.com>
2024-01-18 22:48:47 +0530

    Add files via upload

---
+++ escape sequence.py
@@ -0,0 +1,7 @@
+# hestage comment for a python
+# single line comment 
+print("Hey I am a \"good boy\"\nand my friend is good")
+'multiple line for comment in python'
+"double cout use comment in python"
+print("Hey",4,8,sep="~",end="009\n")
+print("Anuj")
