02561ec7e76f1c5b0e897584086afab2ae9614a9
Anuj Gupta <149232244+anujgupta845@users.noreply.github.com>
2024-01-21 16:40:50 +0530

    add

---
+++ typecasting.py
@@ -0,0 +1,15 @@
+# explicit type conversion
+a = 5
+b = "2"
+sum_string = int(b)
+sum = a + sum_string
+print("this type of explicit",sum)
+# implicit type conversion
+a = 5
+print(type(a))
+b = 2.3
+print(type(b))
+sum = a + b
+print(sum)
+print(type(sum))
+
