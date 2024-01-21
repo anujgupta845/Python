5e415c38fa9d37d9dae70dba7447765e61f785df
Anuj Gupta <149232244+anujgupta845@users.noreply.github.com>
2024-01-21 18:01:35 +0530

    added

---
+++ string_1.py
@@ -0,0 +1,14 @@
+name = "Harry"
+friend = "Rohan"
+anotherFriend = 'Lovish'
+apple = '''He said,
+Hi Harry
+hey I am good
+"I want to eat an apple'''
+print("Hello," + name)
+# print (apple)
+print (name[0])
+print(name[1])
+print (name[2])
+print(name [3] )
+print(name[4])
