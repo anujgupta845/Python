e339ef6e6a5a09522877657433513e8861261192
Anuj Gupta <149232244+anujgupta845@users.noreply.github.com>
2024-01-21 13:53:36 +0530

    add

---
+++ calculator.py
@@ -0,0 +1,8 @@
+a = 20
+b = 10
+print("the type of",a," + ",10, "is", a + b)
+print("the type of",a," - ",10, "is", a - b)
+print("the type of",a," * ",10, "is", a * b)
+print("the type of",a," / ",10, "is", a / b)
+print("the type of",a," % ",10, "is", a % b)
+print("the type of",a," // ",10, "is", a // b)
