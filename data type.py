d3a4bafc53e8292c0f65610653fa60381600c5ed
Anuj Gupta <149232244+anujgupta845@users.noreply.github.com>
2024-01-21 13:30:09 +0530

    add

---
+++ data type.py
@@ -0,0 +1,16 @@
+a=45
+b="anuj"
+c=True
+d=None
+e=complex(8,2)
+print("the type of is",type(a))
+print("the type of is",type(b))
+print("the type of is",type(c))
+print("the type of is",type(d))
+print("the type of is",type(e))
+list1=[8,2.3, [-4,5],["apple","banana"]]
+print(list1)
+tuple1=("lion","horse",("parrot","peacock"),(3,4))
+print(tuple1)
+dict1 = {"Name":"ANUJ","Age":20,"CanVote":"True"}
+print(dict1)
