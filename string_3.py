8e7819bd14109adfc717c0866f73f0b0d62bd86d
Anuj Gupta <149232244+anujgupta845@users.noreply.github.com>
2024-01-22 11:37:04 +0530

    Add files via upload

---
+++ string_3.py
@@ -0,0 +1,39 @@
+# strings are immutable
+a = "!!!Harry!! !!!!!!!!! Harry"
+print(len(a))
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.rstrip ("!"))
+print(a.replace("Harry", "John"))
+print(a.split(" "))
+blogHeading = "introduction to js"
+print(blogHeading.capitalize())
+str1 = "Welcome to the Console!!!"
+print(len(str1))
+print(len(str1.center (50)))
+print (a.count("Harry"))
+str1 = "Welcome to the Console !!!"
+print (str1.endswith("!!!"))
+str1 = "Welcome to the Console !!!"
+print (str1.endswith("to", 4, 10))
+str1 = "He's name is Dan. He is anhonest man."
+11
+print (str1.find("is"))
+# print (str1.index( "ishh"))
+str1 = "Welcome To The Console"
+print (str1.isalnum())
+str1 = "Welcome"
+print (str1.isalpha())
+str1 = "hello world"
+print(str1.islower())
+str1 = "We wish you a MerryChristmas\n"
+print (str1.isprintable())
+str1 = "     " #using Spacebar
+print(str1.isspace())
+str1 = "Python is a InterpretedLanguage"
+print (str1.startswith("Python"))
+str1 = "Python is a InterpretedLanguage"
+print (str1.swapcase())
+str1= "His name is Dan. Dan isan honest man."
+print(str1.title())
